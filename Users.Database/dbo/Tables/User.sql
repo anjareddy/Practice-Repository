99cde7d9d9504cfa338f898870ef76eac43a564d
anjareddy <gaddamreddy9@gmail.com>
2023-07-07 08:27:18 -0400

    Adding Database project

---
+++ Users.Database/dbo/Tables/User.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]                           INT           IDENTITY (1, 1) NOT NULL,
+    [Name]                         VARCHAR (500) NULL,
+    [EmailId]                      VARCHAR (500) NULL,
+    [MobileNumber]                 VARCHAR (500) NULL,
+    [EmergencyContactName]         VARCHAR (500) NULL,
+    [EmergencyContactEmailId]      VARCHAR (500) NULL,
+    [EmergencyContactMobileNumber] VARCHAR (500) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
